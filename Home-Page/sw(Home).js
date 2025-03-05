abb26ba66249265161e548fe563131e0a38d03bf
Haruka1123 <juamigz.arimohanan@gmail.com>
2025-03-05 14:53:36 +1100

    Update sw(Home).js

--- Home-Page/sw(Home).js
+++ Home-Page/sw(Home).js
@@ -1,9 +1,9 @@
 const CACHE_NAME = 'home-page-cache-v1';
 const urlsToCache = [
     '/'
-    '/index-2.html', // Home Page
-    '/styles(Home).css',   // CSS file
-    '/script(Home).js',      // JavaScript file for service worker registration
+    'Home-Page/index-2.html', // Home Page
+    'Home-Page/styles(Home).css',   // CSS file
+    'Home-Page/script(Home).js',      // JavaScript file for service worker registration
     // Add any other assets you want to cache (images, etc.)
 ];
 
